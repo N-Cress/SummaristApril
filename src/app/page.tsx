3d4743ac15582f3f6ff3003726db2a69084b3f68
Noah <azsnobird1227@yahoo.com>
2025-05-11 18:22:57 -0500

    added two sets of content to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex flex-col">
+      <div className="flex h-full overflow-scroll flex-col">
         <div className="flex w-full justify-around mt-4">
           <Image src="/logo.png" className="pl-4 sidebar_image mb-10 " alt="Summarist Logo" height={140} width={240} />
           <div className="pl-4 pt-2 text-lg flex font-sem gap-5">
@@ -79,17 +79,52 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div>
+        <div className="flex w-full mt-10 pl-70 pr-70 justify-around">
           <div>
-            <div> Enhance your knowledge</div>
-            <div> Enhance your knowledge</div>
-            <div> Enhance your knowledge</div>
-            <div> Enhance your knowledge</div>
-            <div> Enhance your knowledge</div>
-            <div> Enhance your knowledge</div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Enhance your knowledge</div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Achieve greater success</div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Improve your health </div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Develop better parenting skills </div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Increase happiness</div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Be the best version of yourself!</div>
+          </div>
+          <div className="flex gap-8 flex-col items-center justify-center bg-[#F1F6F4] p-10">
+            <div className="flex justify-start">
+              <div className="pr-2 text-xl font-bold text-blue-600"> 93% </div>
+              <div className="text-xl w-100"> of Summarist members <span className="font-bold"> significantly increase</span> reading frequency. </div>
+            </div>
+             <div className="flex ">
+              <div className="pr-2 text-xl font-bold text-blue-600"> 96% </div>
+              <div className="text-xl w-100"> of Summarist members <span className="font-bold"> establish better </span> habits. </div>
+            </div>
+             <div className="flex ">
+              <div className="pr-2 text-xl font-bold text-blue-600"> 90% </div>
+              <div className="text-xl w-100"> have made <span className="font-bold"> significant positive </span> change to their lives. </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full mt-10 pl-70 pr-70 items-center justify-around">
+          <div className="flex gap-8 flex-col items-center justify-center bg-[#F1F6F4] p-10">
+            <div className="flex justify-start">
+              <div className="pr-2 text-xl font-bold text-blue-600"> 91% </div>
+              <div className="text-xl w-100"> of Summarist members <span className="font-bold"> report feeling more productive </span>  after incorporating the service into their daily routine. </div>
+            </div>
+             <div className="flex ">
+              <div className="pr-2 text-xl font-bold text-blue-600"> 94% </div>
+              <div className="text-xl w-100"> of Summarist members have <span className="font-bold"> noticed an improvement</span>  in their overall comprehension and retention of information. </div>
+            </div>
+             <div className="flex ">
+              <div className="pr-2 text-xl font-bold text-blue-600"> 88% </div>
+              <div className="text-xl w-100"> of Summarist members <span className="font-bold"> feel more informed </span>  about current events and industry trends since using the platform. </div>
+            </div>
           </div>
           <div>
-
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Accomplish your goals</div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Strengthen your vitality </div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Expand your learning </div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Become a better caregiver </div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Improve your mood </div>
+            <div className="text-[#6B757B] font-semibold text-3xl mb-4"> Maximize your abilities </div>
           </div>
         </div>
       </div>
