d67bbd504e91d1caa0c2c74ff4a054b580cf77db
Noah <azsnobird1227@yahoo.com>
2025-05-14 15:11:23 -0500

    made it so search and x change properly, changed from height to max height for search results

--- src/app/components/sidebarSearch.tsx
+++ src/app/components/sidebarSearch.tsx
@@ -58,8 +58,8 @@ export default function SidebarSearch() {
         <div className="border-b-[#E1E7EA] border-b-1 h-1/12 pr-60">
             <div className="flex h-full items-center w-full justify-end relative">
                 <input value={query} onChange={(e) => setQuery(e.target.value)} className="border-[#E1E7EA] p-2 border-1 h-10 rounded-r-none text-sm pl-4 rounded-xl pr-42" placeholder="Search for books"/> 
-                {!bookData ? <IoIosSearch className="border-[#E1E7EA] border-1 rounded-l-none h-10 w-10 p-2 cursor-pointer rounded-xl"/> : <div onClick={resetQuery} className="border-[#E1E7EA] cursor-pointer border-1 text-xl flex items-center justify-center font-semibold rounded-l-none h-10 w-10 p-2 rounded-xl"> X </div> }
-                <div className={`border-1 border-gray-200 z-100 bg-white absolute top-18 overflow-y-scroll ${bookData ? `h-200` : ``}`}>
+                {!query ? <IoIosSearch className="border-[#E1E7EA] border-1 rounded-l-none h-10 w-10 p-2 cursor-pointer rounded-xl"/> : <div onClick={resetQuery} className="border-[#E1E7EA] cursor-pointer border-1 text-xl flex items-center justify-center font-semibold rounded-l-none h-10 w-10 p-2 rounded-xl"> X </div> }
+                <div className={`border-1 border-gray-200 z-100 bg-white absolute top-18 overflow-y-scroll ${query ? `max-h-200` : ``}`}>
                 {bookData && query && bookData.map((book) => (
                       <Link href={`/books/${book.id}`} key={book.id} className="hover:bg-gray-200 shrink-0 p-4 w-100 items-center flex  ml-4 mr-4 ">
                         <Image src={book.imageLink} alt={book.title} width={100} height={100} className=" h-20 pr-4" />
