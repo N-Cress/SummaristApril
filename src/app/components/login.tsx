9e2eb6dbf6e561efc52c13be4594c3753debd921
Noah <azsnobird1227@yahoo.com>
2025-05-15 12:37:02 -0500

    sign up functionality

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 import { app } from '../firebase';
 import { getFirestore, doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
@@ -8,6 +8,8 @@ import { RootState } from '../../lib/store';
 import { setLogged, setUEmail, setUName } from '@/lib/features/loggedSlice';
 import { setLogging } from "@/lib/features/loggingSlice";
 import { setSub } from "@/lib/features/subSlice";
+import { FcGoogle } from "react-icons/fc";
+
 
 const db = getFirestore();
 
@@ -17,6 +19,7 @@ const auth = getAuth(app);
 const Login = ({ }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [signing, setSigning] = useState(false);
     const [error, setError] = useState<boolean>(false);
 
     const logged = useSelector((state: RootState) => state.persisted.logged.value)
@@ -27,6 +30,20 @@ const Login = ({ }) => {
     
 
     const user = auth.currentUser;
+
+    const handleSignup = async (email: string, password: string) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      dispatch(setLogged(true));
+      dispatch(setUEmail(user.email));
+      dispatch(setLogging(false));
+      setSigning(false);
+    } catch (error: any) {
+      console.error('Login failed:', error);
+      setError(true);
+    }
+  };
 
       async function userPremium() {
         if (!user) {
@@ -86,6 +103,46 @@ const Login = ({ }) => {
       className="fixed inset-0 bg-black/75  flex items-center justify-center z-50"
       onClick={handleBgClick}
       >
+      {signing ? <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-lg w-96">
+      <h2 className="text-xl font-semibold mb-4">Sign up to Summarist</h2>
+        <button onClick={signInWithGoogle} className="w-full cursor-not-allowed mt-2 mb-2 bg-[#4285F4] text-white pl-3 items-center flex py-2 rounded">
+          <div className="bg-white p-1 rounded-lg">
+            <FcGoogle size={20}/>
+          </div>
+          <div className="pl-14"> Sign up with Google </div>
+        </button>
+        <div className="mt-2 mb-4 flex w-full items-center justify-center"> 
+          <div className="border-1 border-gray-300 w-full mr-5"> </div>
+          <div> or </div>
+          <div className="border-1 border-gray-300 w-full ml-5 "> </div>
+        </div>
+      <form
+        className="flex flex-col items-center bg-white pl-8 pr-8 g w-96"
+        onClick={handleBoxClick}
+        onSubmit={(e) => {e.preventDefault(); handleSignup(email, password);}}
+      >
+       
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="border p-2 w-full mb-4 rounded"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-2 w-full mb-4 rounded"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="w-full bg-[#2BD97C] text-black cursor-pointer py-2 mb-4 rounded">
+          Sign up
+        </button>
+        {error && <div className="text-red-500 mt-2"> Invalid Email or Password </div>}
+        <div onClick={() => {setSigning(!signing);
+            setError(false);}} className="cursor-pointer text-[#1191E9]"> Already have an account? </div>
+      </form>
+      </div>
+      : 
       <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-lg w-96">
       <h2 className="text-xl font-semibold mb-4">Log in to Summarist</h2>
         <button onClick={() => handleLogin('test@email.com', "123456")} className="cursor-pointer mt-2 mb-2 w-full bg-[#25396B] text-white py-2 rounded">
@@ -127,9 +184,12 @@ const Login = ({ }) => {
         </button>
         {error && <div className="text-red-500 mt-2"> Invalid Email or Password </div>}
         <div className="text-[#1191E9] mt-4 mb-2 cursor-not-allowed"> Forgot your password?</div>
-        <div className="text-[#1191E9]"> Don't have an account? </div>
+        <div onClick={() => {setSigning(!signing);
+          setError(false);
+        }} className="cursor-pointer text-[#1191E9]"> Don't have an account? </div>
       </form>
-      </div>
+      </div>}
+      
     </div>
     )
 }
